32678a794ee6a2ec4fa943c206fc0315fb01fd4c
Nicholas <berube.nicholas@gmail.com>
2021-08-14 14:26:37 -0400

    Renamed ConsumeConsole task

--- examples/KafkaConsumeConsole/Tasks/ConsumeMessagesTask.cs
+++ examples/KafkaConsumeConsole/Tasks/ConsumeMessagesTask.cs
@@ -10,26 +10,27 @@ using System.Threading.Tasks;
 
 namespace KafkaConsumeConsole.Tasks
 {
-    public class ConsumeOrdersCreatedTask : IApplicationTask
+    public class ConsumeMessagesTask : IApplicationTask
     {
         private readonly IMessageConsumer _messageConsumer;
-        private readonly ILogger<ConsumeOrdersCreatedTask> _logger;
+        private readonly ILogger<ConsumeMessagesTask> _logger;
         private readonly TaskOptions _taskOptions;
 
-        public ConsumeOrdersCreatedTask(IMessageConsumer messageConsumer, IOptions<TaskOptions> taskOptions, ILogger<ConsumeOrdersCreatedTask> logger)
+        public ConsumeMessagesTask(IMessageConsumer messageConsumer, IOptions<TaskOptions> taskOptions, ILogger<ConsumeMessagesTask> logger)
         {
             this._messageConsumer = messageConsumer;
             this._taskOptions = taskOptions.Value;
             this._logger = logger;
         }
 
-        public string Name => nameof(ConsumeOrdersCreatedTask);
+        public string Name => nameof(ConsumeMessagesTask);
 
         public async Task RunAsync()
         {
             IAsyncEnumerable<IMessage> orderCreated = this._messageConsumer.ConsumeAllEventsAsync<OrderCreatedEvent>(this._taskOptions.ConsumeWaitDuration);
             IAsyncEnumerable<IMessage> orderCanceled = this._messageConsumer.ConsumeAllEventsAsync<OrderCanceledEvent>(this._taskOptions.ConsumeWaitDuration);
             IAsyncEnumerable<IMessage> messages = orderCreated.Concat(orderCanceled);
+            
             await foreach (var message in messages)
             {
                 this._logger.LogInformation("Event received: {Event}", message.Value);
